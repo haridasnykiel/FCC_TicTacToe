3cc51084be68f1bc2a713b575d076b7a224374c0
Haridas <haridasnykiel@gmail.com>
2018-04-03 22:21:58 +0100

    Added the layouts for the first 2 selection pages

--- gameLogic.js
+++ gameLogic.js
@@ -3,40 +3,59 @@ $(document).ready(function(){
   $('#game_in_play').hide();
   $('#select_nought_or_cross').hide();
 
-
 });
 
 
 var Game = function() {
-  this.PlayerOne = "";
-  this.PlayerTwo = "";
+  this.PlayerOne = new Player();
+  this.PlayerTwo = new Player();
   this.eventListeners();
+}
+
+var Player = function() {
+  this.IsBot = false;
+  this.PlaySymbol = "";
 }
 
 Game.prototype.eventListeners = function() {
   $("#button_players_one").click(function(event){
     event.preventDefault();
-    setPlayers("human", "bot");
-  });
+    this.setBotPlayer(true);
+    nextPage(2);
+  }.bind(this));
 
   $("#button_players_two").click(function(event){
     event.preventDefault();
-    setPlayers("human", "human");
-  });
-
-  function setPlayers(playerOne, playerTwo) {
-    this.PlayerOne = playerOne;
-    this.PlayerTwo = playerTwo;
     nextPage(2);
-  }
+  }.bind(this));
+
+  $(".symbol").click(function(event){
+    event.preventDefault();
+    this.setSymbols($(".symbol").val());
+    nextPage(3);
+  }.bind(this));
 
 }
 
+Game.prototype.setSymbols = function(symbolName) {
+  if(symbolName == "cross") {
+    this.PlayerOne.PlaySymbol = "cross"
+    this.PlayerTwo.PlaySymbol = "nought"
+  } else if(symbolName == "nought") {
+    this.PlayerOne.PlaySymbol = "nought"
+    this.PlayerTwo.PlaySymbol = "cross"
+  }
+}
+
+Game.prototype.setBotPlayer = function(isBot) {
+  this.PlayerTwo.IsBot = isBot;
+}
+
 function nextPage(pageNum) {
-  if(pageNum = 2) {
+  if(pageNum == 2) {
     $('#number_player_selection').hide(400);
     $('#select_nought_or_cross').show(400);
-  } else if(pageNum = 3) {
+  } else if(pageNum == 3) {
     $('#game_in_play').show(400);
     $('#select_nought_or_cross').hide(400);
   }
