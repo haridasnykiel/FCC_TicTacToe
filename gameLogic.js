e4823b3efb7e5cd7f6bbdcc6aa37a6f36a606cee
Haridas <haridasnykiel@gmail.com>
2018-04-08 16:57:22 +0100

    Working on the logic for the tic tac toe project. Haviong an issue with adding the correct in image to the div that has been clicked on the board

--- gameLogic.js
+++ gameLogic.js
@@ -7,16 +7,18 @@ $(document).ready(function(){
 
 });
 
-
 var Game = function() {
   this.PlayerOne = new Player();
   this.PlayerTwo = new Player();
+  this.WhosTurn = "One";
+  this.winningCombinations = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
   this.eventListeners();
 }
 
 var Player = function() {
   this.IsBot = false;
   this.PlaySymbol = "";
+  this.Moves = [];
 }
 
 Game.prototype.eventListeners = function() {
@@ -37,20 +39,33 @@ Game.prototype.eventListeners = function() {
     nextPage(3);
   }.bind(this));
 
+  $(".play").click(function(event){
+    event.preventDefault();
+    this.play();
+  }.bind(this));
+
 }
 
 Game.prototype.setSymbols = function(symbolName) {
-  if(symbolName == "cross") {
-    this.PlayerOne.PlaySymbol = "cross"
-    this.PlayerTwo.PlaySymbol = "nought"
-  } else if(symbolName == "nought") {
-    this.PlayerOne.PlaySymbol = "nought"
-    this.PlayerTwo.PlaySymbol = "cross"
+  if(symbolName == "X") {
+    this.PlayerOne.PlaySymbol = "X"
+    this.PlayerTwo.PlaySymbol = "O"
+  } else if(symbolName == "O") {
+    this.PlayerOne.PlaySymbol = "O"
+    this.PlayerTwo.PlaySymbol = "X"
   }
 }
 
 Game.prototype.setBotPlayer = function(isBot) {
   this.PlayerTwo.IsBot = isBot;
+}
+
+Game.prototype.play = function(elementValue) {
+  if(this.WhosTurn = "One") {
+    if(this.PlayerOne.PlaySymbol == "X") {
+      $('.play :input[value='+elementValue+']').prepend('<img src="./images/cross.png" />');
+    }
+  }
 }
 
 function nextPage(pageNum) {
@@ -60,7 +75,8 @@ function nextPage(pageNum) {
   } else if(pageNum == 3) {
     $('#game_in_play').show(400);
     $('#select_nought_or_cross').hide(400);
-    $('#p_one').animate({opacity: 0.9 }, 400 ); // This line is not being called. Requires investigation.
+    $('.player_turn').animate({opacity: 0.9 }, 400 );
+    $('#p_two').animate({opacity: 0 }, 50 );
   }
 
 }
